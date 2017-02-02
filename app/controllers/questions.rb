fe74bf897e1b1b949d91f16a36d7d7cf77b2a655
Kendy Ng <email": null>
2017-02-02 17:09:26 -0500

    connect question show page with erb

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,7 +3,12 @@ get "/questions" do
   @questions = Question.all
   erb :"questions/index"
 end
+
 #get one question record
+get "/questions/:id" do
+  @question = Question.find_by_id(params[:id])
+  erb :"questions/show"
+end
 #get the form for a new question
 #post a new question
 #edit a specific question record
