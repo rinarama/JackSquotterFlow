e15bd6c4391e9f79935f0aebc4d8b3496c6f905b
Kendy Ng <email": null>
2017-02-02 17:54:16 -0500

    add post route for new questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -9,13 +9,24 @@ get "/questions/new" do
   erb :"questions/new"
 end
 
+#post a new question
+post "/questions" do
+  # need to change User.first to current_user
+  question = User.first.questions.new(params[:question])
+
+  if question.save
+    redirect "/questions"
+  else
+    # @errors = question.errors.full_messages
+    erb :"questions/new"
+  end
+end
+
 #get one question record
 get "/questions/:id" do
   @question = Question.find_by_id(params[:id])
   erb :"questions/show"
 end
 
-
-#post a new question
 #edit a specific question record
 #delete a specifc question record
