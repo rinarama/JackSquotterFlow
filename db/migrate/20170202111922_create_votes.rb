9f5b8b40a4f78c190576895962be8fad44f69de8
Kendy Ng & Leila Gleich & Rina Robinson <pair+kenobi82403.leilagleich.rinarama@devbootcamp.com>
2017-02-02 11:26:43 -0500

    Populate migration tables

--- db/migrate/20170202111922_create_votes.rb
+++ db/migrate/20170202111922_create_votes.rb
@@ -1,4 +1,13 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+    create_table :votes do |t|
+      t.integer :value_status
+      t.references :user, null: false
+
+      t.integer :votable_id
+      t.string :votable_type
+
+      t.timestamps(null: false)
+    end
   end
 end
--- db/migrate/20170202111927_create_comments.rb
+++ db/migrate/20170202111927_create_comments.rb
@@ -1,4 +1,13 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.string :comment, :null => false
+      t.references :user
+
+      t.integer :commentable_id
+      t.string :commentable_type
+
+      t.timestamps(null: false)
+    end
   end
 end
