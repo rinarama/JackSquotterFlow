ce2909400bbbfce9bcead2928208588cbc7b90bf
[01;31m[K"name"[m[K: null <[01;31m[K"email"[m[K: null>
2017-02-02 16:52:38 -0500

    Create user login and register views

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,7 @@ post '/users' do
   @user = User.new(params[:user])
   if @user.save
     session[:user_id] = @user.id
-    redirect "/users/#{@user.id}/profile"
+    redirect "/users/#{@user.id}"
   else
     @errors = @user.errors.full_messages
     erb :'/users/new'
@@ -18,12 +18,12 @@ get '/users/login' do
 end
 
 post '/users/login' do
-  @user = User.find_by(email: params[:user][:email])
+  @user = User.find_by(username: params[:user][:username])
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect "/"
   else
-    @errors = ["Invalid email or password"]
+    @errors = ["Invalid username or password"]
     erb :'users/login'
   end
 end
