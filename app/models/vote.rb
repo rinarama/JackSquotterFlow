fa66b3a8e4207ba4a3d5786f4f3c062f5e19da84
Kendy Ng & Leila Gleich & Rina Robinson <pair+kenobi82403.leilagleich.rinarama@devbootcamp.com>
2017-02-02 12:33:21 -0500

    Test validations and include tentative numericality for vote status

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,5 +4,5 @@ class Vote < ActiveRecord::Base
 
   validates :vote_status, :user_id, :votable_id, :votable_type, presence: true
   validates :user_id, uniqueness: {scope: [:votable_id, :votable_type]}
-  validates :vote_status
+  validates :vote_status, numericality: { only_integer: true, greater_than_or_equal_to: -1, less_than_or_equal_to: 1 }
 end
--- db/migrate/20170202111922_create_votes.rb
+++ db/migrate/20170202111922_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :value_status
+      t.integer :vote_status
       t.references :user, null: false
 
       t.integer :votable_id
