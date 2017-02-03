6ed7e702cef9c6e26172fdf0ebdf10eed6863451
Rina Robinson <email": null>
2017-02-03 15:42:13 -0500

    Corrrect ruote to edit form

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -2,33 +2,23 @@ before "/answers/*" do
   require_user
 end
 
-# "/questions/:id/comments"
-# redirect "/questions/#{params[:id]}"
-# "/questions/:id/answers/:id/comments"
-
 get "/questions/:id/answers" do
   @answers = @questions.answers
   erb :"answers/", :locals => {:answer => params[:answer]}
 end
 
-get "/answers/new" do
-  # if request.xhr?
-  #   erb :'answers/new', layout: false
-  # else
+get "/questions/:id/answers/new" do
+  @question = Question.find_by_id(params[:id])
     erb :"answers/new"
-  # end
 end
 
-post "/answers" do
-  answer = Answer.new(params)
-  if answer.save
-    # if request.xhr?
-    #   erb :'answers/_one_answer', layout: false, locals:{one_answer: answer}
-    # else
+post "/questions/:id/answers" do
+  @question = Question.find_by_id(params[:id])
+  @answer = @question.answers.new(answer: params[:answer], user_id: current_user.id)
+  if @answer.save
       redirect "/questions/#{params[:id]}"
-    # end
   else
-    @errors = answer.errors.full_messages
+    @errors = @answer.errors.full_messages
     erb :"answers/new"
   end
 end
@@ -39,21 +29,23 @@ get "/questions/:id/answers" do
 end
 
 get "/questions/answers/:id/edit" do
-  @answer = Answer.find_by_id(id: params[:id])
+  @answer = Answer.find_by_id(params[:id])
   erb :"/answers/edit"
 end
 
-put "/answers/:id" do
-  @answer = Answer.find_by_id(params[:id])
+put "/questions/:id/answers/:id" do
+  @question = Question.find_by_id(params[:id])
+  @answer = @question.answers.find_by_id(params[:id])
+  binding.pry
   if @answer.update(params[:answer])
-    redirect "/answers/#{@answer.id}"
+    redirect "/questions/#{params[:id]}"
   else
     @errors = @answer.errors.full_messages
-    erb :"/answers/edit"
+    erb :"/answers/new"
   end
 end
 
 delete "/questions/:id/answers/:id" do
   Answer.find_by_id(params[:id]).destroy
-  redirect "/answers"
+  redirect "/questions/#{params[:id]}"
 end
