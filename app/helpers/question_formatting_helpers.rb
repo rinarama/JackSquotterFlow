1dd2645a615416dc0cd3f104c19ba28ad691c3d2
Kendy Ng <email": null>
2017-02-02 20:46:35 -0500

    add css to questions

--- app/helpers/question_formatting_helpers.rb
+++ app/helpers/question_formatting_helpers.rb
@@ -1,20 +1,20 @@
 module EntryFormatting
   def stub(text)
-    return text if less_than_26_words_long?(text)
-    first_25_words(text) +  '...'
+    return text if less_than_16_words_long?(text)
+    first_15_words(text) +  '...'
   end
 
   private
-  def less_than_26_words_long?(text)
-    !(text.match first_25_words_regex)
+  def less_than_16_words_long?(text)
+    !(text.match first_15_words_regex)
   end
 
-  def first_25_words(text)
-    text.slice first_25_words_regex
+  def first_15_words(text)
+    text.slice first_15_words_regex
   end
 
-  def first_25_words_regex
-    /\A(\S+\s){25}/
+  def first_15_words_regex
+    /\A(\S+\s){15}/
   end
 end
 
