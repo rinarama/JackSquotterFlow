5b2169d975bfce44ee54886da41203b5cf56ff38
Leila Gleich <email": null>
2017-02-03 15:57:09 -0500

    Create login ajax partials

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#login-link").on("click", function(e){
+      e.preventDefault();
+      url = $(e.target).attr("href")
+      $this = $(this)
+      $.ajax({
+        method: 'get',
+        url: url
+      }).success(function(response){
+        $("#new-note-form").append(response);
+        $this.hide()
+      });
+    });
 });
