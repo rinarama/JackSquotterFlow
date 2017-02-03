fe13b8026c73eff54f5fd10c9c5820a7c12ba34f
Leila Gleich <email": null>
2017-02-03 17:14:15 -0500

    Ajax nav bar

--- public/js/application.js
+++ public/js/application.js
@@ -9,14 +9,14 @@ $(document).ready(function() {
         $("#user-auth").html(response);
       });
     });
-    $(".standard-form").on("submit", function(e){
-        e.preventDefault();
-        url = $(e.target).attr("action");
-        $.ajax({
-          method: 'get',
-          url: url
-        }).success(function(response){
-          $(".box").hide()
-        });
+  $("#login-link").on("click", function(e){
+      e.preventDefault();
+      url = $(e.target).attr("href");
+      $.ajax({
+        method: 'get',
+        url: url
+      }).success(function(response){
+        $("#user-auth").html(response);
       });
+    });
 });
