65aa6939e3455d33d69f6bd4a4aefc89ad61765a
Kendy Ng <email": null>
2017-02-02 19:00:47 -0500

    connect errors and add user auth to questions

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -6,18 +6,19 @@ end
 
 #get the form for a new question
 get "/questions/new" do
+  require_user
   erb :"questions/new"
 end
 
 #post a new question
 post "/questions" do
-  # need to change User.first to current_user
-  question = User.first.questions.new(params[:question])
+  require_user
+  question = current_user.questions.new(params[:question])
 
   if question.save
     redirect "/questions"
   else
-    # @errors = question.errors.full_messages
+    @errors = question.errors.full_messages
     erb :"questions/new"
   end
 end
@@ -30,24 +31,26 @@ end
 
 #edit a specific question record
 get "/questions/:id/edit" do
+  require_user
   @question = Question.find_by_id(params[:id])
   erb :"/questions/edit"
 end
 
 put "/questions/:id" do
-  # need to change User.first to current_user
-  question = Question.find_by_id(params[:id])
+  require_user
+  @question = Question.find_by_id(params[:id])
 
-  if question.update(params[:question])
+  if  @question.update(params[:question])
     redirect "/questions/#{question.id}"
   else
-    # @errors = question.errors.full_messages
+    @errors = @question.errors.full_messages
     erb :"/questions/edit"
  end
 end
 
 #delete a specifc question record
 delete "/questions/:id" do
+  require_user
   Question.find_by_id(params[:id]).destroy
 
   redirect "/questions"
