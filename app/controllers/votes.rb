bcc91a81d068838483358a8e664ac4ffe4f285c1
Leila Gleich <email": null>
2017-02-03 14:49:06 -0500

    Allow user to up and downvote with no takebacks

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,11 +1,15 @@
 post '/questions/:question_id/upvote' do
   @question = Question.find(params[:question_id])
-  @question.votes..update_or_create( user: current_user, vote_status: 1 )
+  @question.votes.update_or_create( user: current_user, vote_status: 1 )
     redirect "questions/#{@question.id}"
 end
 
 post '/questions/:question_id/downvote' do
   @question = Question.find(params[:question_id])
-  @question.votes..update_or_create( user: current_user, vote_status: -1 )
+  @question.votes.update_or_create( user: current_user, vote_status: -1 )
     redirect "questions/#{@question.id}"
 end
+
+#find or initialize?
+# http://apidock.com/rails/v4.0.2/ActiveRecord/Relation/find_or_initialize_by
+#save if there is no user that already voted
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,4 +5,15 @@ class Vote < ActiveRecord::Base
   validates :vote_status, :user_id, :votable_id, :votable_type, presence: true
   validates :user_id, uniqueness: {scope: [:votable_id, :votable_type]}
   validates :vote_status, numericality: { only_integer: true, greater_than_or_equal_to: -1, less_than_or_equal_to: 1 }
+
+  def self.update_or_create(attributes)
+    assign_or_new(attributes).save
+  end
+
+  def self.assign_or_new(attributes)
+    obj = first || new
+    obj.assign_attributes(attributes)
+    obj
+  end
+
 end
