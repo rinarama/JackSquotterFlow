f7a157ef3460cd551edde3829684d511da03b458
Rina Robinson <email": null>
2017-02-03 11:18:39 -0500

    Fix routes for answers

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -2,59 +2,58 @@ before "/answers/*" do
   require_user
 end
 
-#show all answsers
-#get one question record
-#get the form for a new question
-#post a new question
-#edit a specific question record
-#delete a specifc question re cord
-get '/answers' do
+# "/questions/:id/comments"
+# redirect "/questions/#{params[:id]}"
+# "/questions/:id/answers/:id/comments"
+
+get "/questions/:id/answers" do
   @answers = Answer.all
-  erb :'answers/index'
+  erb :"answers/"
 end
 
-get '/answers/new' do
+get "/answers/new" do
   # if request.xhr?
   #   erb :'answers/new', layout: false
   # else
-    erb :'answers/new'
+    erb :"answers/new"
   # end
 end
 
-post '/answers' do
+post "/answers" do
   answer = Answer.new(params)
   if answer.save
     # if request.xhr?
     #   erb :'answers/_one_answer', layout: false, locals:{one_answer: answer}
     # else
-      redirect '/answers'
+      redirect "/questions/#{params[:id]}"
     # end
   else
     @errors = answer.errors.full_messages
-    erb :'answers/new'
+    erb :"answers/new"
   end
 end
 
-get '/answers/:id' do
-  @answer = Answer.find(params[:id])
-  erb :'answers/show'
+get "/questions/:id/answers" do
+  @answer = Answer.find_by_id(params[:id])
+  erb :"answers/show"
 end
 
-get '/answers/:id/edit' do
-  @answer = Answer.find_by(id: params[:id])
-  erb :'/answers/edit'
+get "/answers/:id/edit" do
+  @answer = Answer.find_by_id(id: params[:id])
+  erb :"/answers/edit"
 end
 
-put '/answers/:id' do
-  @answer = Answer.find(params[:id])
+put "/answers/:id" do
+  @answer = Answer.find_by_id(params[:id])
   if @answer.update(params[:answer])
     redirect "/answers/#{@answer.id}"
   else
     @errors = @answer.errors.full_messages
-    erb :'/answers/edit'
+    erb :"/answers/edit"
   end
 end
 
-delete '/answers/:id' do
-
+delete "/answers/:id" do
+  Answer.find_by_id(params[:id]).destroy
+  redirect "/answers"
 end
