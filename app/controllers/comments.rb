7010f38dcbdb362da3f2fef2b2ead2fba646f7c8
Kendy Ng <email": null>
2017-02-03 11:12:29 -0500

    add index route for comments

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,6 +1,18 @@
 #show all comments
+get "/questions/:id/comments" do
+  redirect "questions/#{params[:id]}"
+end
+
 #get one comment record
+
+
 #get the form for a new comment
+
+
 #post a new comment
+
+
 #edit a specific comment record
+
+
 #delete a specifc comment record
